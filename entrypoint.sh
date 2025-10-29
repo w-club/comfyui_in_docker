5ab65c3dc77c65877349fe59713275d71595a5b9
w-club <w@w-w.club>
2025-10-30 01:00:48 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Use provided PUID and PGID, or default to 1000 if not set
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+USER_NAME="appuser"
+USER_HOME="/home/${USER_NAME}"
+
+# Create a group and user with the specified PUID and PGID
+if ! getent group ${PGID} >/dev/null; then
+    groupadd -g ${PGID} ${USER_NAME}
+fi
+
+if ! id -u ${PUID} >/dev/null 2>&1; then
+    useradd -u ${PUID} -g ${PGID} -m -d ${USER_HOME} -s /bin/bash ${USER_NAME}
+    chown -R ${PUID}:${PGID} /comfyui /workspace
+fi
+
+# Set the correct PATH for the user
+export PATH="${USER_HOME}/.local/bin:$PATH"
+export PUID PGID
+
+# Mark directories as safe for Git
+git config --global --add safe.directory ${COMFY}
+
+# Run the app
+echo "Running ComfyUI..."
+exec /workspace/run_comfy.sh
